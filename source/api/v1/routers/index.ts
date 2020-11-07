22006244a45ecf37febfd5056a198138adc28656
nicolatoscan <nicola.toscan>
2020-11-07 20:55:15 +0100

    feat: user authentication and signup and login endpoints

--- source/api/v1/routers/index.ts
+++ source/api/v1/routers/index.ts
@@ -1,9 +1,11 @@
 import * as express from 'express';
 import { auth } from '../../../services/auth.services';
 import getUserRouter from './users.router';
+import getAuthRouter from './auth.router';
 
 export default function(): express.Router {
     const router = express.Router();
+    router.use('/auth', getAuthRouter());
     router.use(auth);
     router.use('/users', getUserRouter());
     return router;
--- source/services/auth.services.ts
+++ source/services/auth.services.ts
@@ -2,17 +2,21 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 export function auth(req: Request, res: Response, next: NextFunction): void {
-    const token = req.header('Authorization')?.split(' ');
-    if (!token || token.length !== 2) {
+    const token = req.header('Authorization');
+    if (token) {
+        try {
+            res.locals = jwt.verify(token, process.env.TOKEN_SECRET as string) as Record<string, unknown>;
+            next();
+        } catch (err) {
+            res.status(301).send('Access denied.\nInvalid Token');
+        }
+    } else {
         res.status(301).send('Access denied.\nNo token found');
-        return;
     }
+}
 
-    try {
-        res.locals = jwt.verify(token[1], process.env.TOKEN_SECRET as string) as Record<string, unknown>;
-        next();
-    } catch (err) {
-        res.status(301).send('Access denied.\nInvalid Token');
-    }
+export function addAuthToResponse(res: Response, userInfo: { username: string }): Response {
+    const token = jwt.sign(userInfo, process.env.TOKEN_SECRET as string);
+    return res.header('Authentication', token);
 }
 
