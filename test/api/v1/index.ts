acd060b5925c3d58bfa19cdc1c89f6340c82c052
nicolatoscan <nicola.toscan>
2020-11-07 22:06:50 +0100

    test: test for auth models and database services

---
+++ test/api/v1/index.ts
@@ -0,0 +1,7 @@
+import getModelsTests from './models';
+
+export default function(): void {
+    describe('V1', function() {
+        getModelsTests();
+    });
+}
---
+++ test/services/index.ts
@@ -0,0 +1,7 @@
+import databaseServicesTest from './database.services.test';
+
+export default function(): void {
+    describe('Services', function() {
+        databaseServicesTest();
+    });
+}
