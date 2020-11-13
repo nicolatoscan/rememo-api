6708824dcab953c01fbf61a85211a48b218d29e5
alessiogandelli <alessiogandelli99@gmail.com>
2020-11-13 23:22:39 +0100

    feat(users.router.ts): add deleted logged user endpoint

--- source/api/v1/routers/users.router.ts
+++ source/api/v1/routers/users.router.ts
@@ -1,10 +1,11 @@
 import * as express from 'express';
 import databaseService from '../../../services/database.services';
 import * as Models from '../models';
-import { User } from '../models';
+import bcrypt from 'bcryptjs';
 
-async function getUserById(req: express.Request, res: express.Response) {
-    
+
+async function getLoggedUser(req: express.Request, res: express.Response) {
+
     const user = await databaseService.getCollection('users').findOne({ username: res.locals.username }); 
     if (user) { 
         res.send(Models.getUserFromDBDoc(user));
@@ -13,17 +14,31 @@ async function getUserById(req: express.Request, res: express.Response) {
     }
 }
 
+async function deleteLoggedUser(req: express.Request, res: express.Response) {
+    const valUser = Models.validateLoginUser(req.body);
+    if (valUser.error) {
+        return res.status(400).send(valUser.error);
+    } else if(valUser.value){
+        if (valUser.value.username !== res.locals.username) {
+            return res.status(400).send('username and logged user do not match ');
+        }
+        const user = (await databaseService.getCollection('users').findOne({ username: res.locals.username })) as Models.DBUserDoc; 
 
-async function deleteUser(req: express.Request, res: express.Response){
-    const a =  await databaseService.getCollection('users').deleteOne({ 'username' : req.body.username });
+        if(!user || !(await bcrypt.compare(valUser.value.password, user.password))){
+            return res.status(301).send('Wrong password');
+        }
 
-    console.log(a);
+        await databaseService.getCollection('users').deleteOne({ username: res.locals.username });
+        return res.send('User deleted');
+
+    }
 }
+
 
 export default function (): express.Router {
     const router = express.Router();
-    router.get('/', getUserById);
+    router.get('/', getLoggedUser);
+    router.delete('/', deleteLoggedUser);
     
-    router.delete('/delete/:username', deleteUser);
     return router;
 }
