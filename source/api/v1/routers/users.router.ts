5bb22e36833a8e6d5b78b31e316a9aee0e56c0d0
alessiogandelli <alessiogandelli99@gmail.com>
2020-11-13 22:57:05 +0100

    feat(users.router.ts): add get logged user endpoint

--- source/api/v1/routers/users.router.ts
+++ source/api/v1/routers/users.router.ts
@@ -1,33 +1,29 @@
 import * as express from 'express';
 import databaseService from '../../../services/database.services';
 import * as Models from '../models';
+import { User } from '../models';
 
 async function getUserById(req: express.Request, res: express.Response) {
-    const user = await databaseService.getCollection('users').findOne({ username: req.params.username });
+    
+    const user = await databaseService.getCollection('users').findOne({ username: res.locals.username }); 
     if (user) { 
-        res.send(user);
-    }else{
+        res.send(Models.getUserFromDBDoc(user));
+    } else {
         res.status(404).send('User not found');
     }
 }
 
-async function postUser(req: express.Request, res: express.Response) {
-    const valUser = Models.validateUser(req.body);
-    if (valUser.error) {
-        res.status(400).send(valUser.error);
-    } else if (valUser.value) {
-        if (await databaseService.getCollection('users').findOne({ email: valUser.value.email })) {
-            res.status(403).send('Email already used');
-        } else {
-            const insertedId = (await databaseService.getCollection('users').insertOne(valUser.value)).insertedId;
-            res.status(201).send(insertedId);
-        }
-    }
+
+async function deleteUser(req: express.Request, res: express.Response){
+    const a =  await databaseService.getCollection('users').deleteOne({ 'username' : req.body.username });
+
+    console.log(a);
 }
 
 export default function (): express.Router {
     const router = express.Router();
-    router.get('/:username', getUserById);
-    router.post('/', postUser);
+    router.get('/', getUserById);
+    
+    router.delete('/delete/:username', deleteUser);
     return router;
 }
