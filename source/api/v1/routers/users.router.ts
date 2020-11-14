8828643589417d0796d0324257e87007869804b7
alessiogandelli <alessiogandelli99@gmail.com>
2020-11-14 02:19:02 +0100

    feat(users.router.test): add update user endpoint

--- source/api/v1/routers/users.router.ts
+++ source/api/v1/routers/users.router.ts
@@ -6,8 +6,8 @@ import bcrypt from 'bcryptjs';
 
 async function getLoggedUser(req: express.Request, res: express.Response) {
 
-    const user = await databaseService.getCollection('users').findOne({ username: res.locals.username }); 
-    if (user) { 
+    const user = await databaseService.getCollection('users').findOne({ username: res.locals.username });
+    if (user) {
         res.send(Models.getUserFromDBDoc(user));
     } else {
         res.status(404).send('User not found');
@@ -18,27 +18,69 @@ async function deleteLoggedUser(req: express.Request, res: express.Response) {
     const valUser = Models.validateLoginUser(req.body);
     if (valUser.error) {
         return res.status(400).send(valUser.error);
-    } else if(valUser.value){
+    } else if (valUser.value) {
+        //check username
         if (valUser.value.username !== res.locals.username) {
             return res.status(400).send('username and logged user do not match ');
         }
-        const user = (await databaseService.getCollection('users').findOne({ username: res.locals.username })) as Models.DBUserDoc; 
 
-        if(!user || !(await bcrypt.compare(valUser.value.password, user.password))){
+        //check password
+        const user = (await databaseService.getCollection('users').findOne({ username: res.locals.username })) as Models.DBUserDoc;
+        if (!user || !(await bcrypt.compare(valUser.value.password, user.password))) {
             return res.status(301).send('Wrong password');
         }
 
+        //delete
         await databaseService.getCollection('users').deleteOne({ username: res.locals.username });
         return res.send('User deleted');
 
     }
 }
 
+async function updateLoggedUser(req: express.Request, res: express.Response) {
+    const valUser = Models.validateUpdateUser(req.body);
+
+    if (valUser.error) {
+        return res.status(400).send(valUser.error);
+    } else if (valUser.value) {
+
+        //check username
+        if (valUser.value.username !== res.locals.username) {
+            return res.status(400).send('username and logged user do not match ');
+        }
+
+        //check password
+        const user = (await databaseService.getCollection('users').findOne({ username: res.locals.username })) as Models.DBUserDoc;
+        if (!user || !(await bcrypt.compare(valUser.value.password, user.password))) {
+            return res.status(301).send('Wrong password');
+        }
+
+        //update
+        if (valUser.value.displayName) {
+            await databaseService.getCollection('users').updateOne({ username: valUser.value.username }, { $set: { displayName: valUser.value.displayName } });
+        }
+        if (valUser.value.email) {
+            await databaseService.getCollection('users').updateOne({ username: valUser.value.username }, { $set: { email: valUser.value.email } });
+        }
+        if (valUser.value.newPassword) {
+            const salt = await bcrypt.genSalt();
+            const hashedPassword = await bcrypt.hash(valUser.value.newPassword, salt);
+            await databaseService.getCollection('users').updateOne({ username: valUser.value.username }, { $set: { password: hashedPassword } });
+        }
+
+    }
+    return res.send('User updated');
+}
+
+
+
+
 
 export default function (): express.Router {
     const router = express.Router();
     router.get('/', getLoggedUser);
     router.delete('/', deleteLoggedUser);
-    
+    router.put('/', updateLoggedUser);
+
     return router;
 }
