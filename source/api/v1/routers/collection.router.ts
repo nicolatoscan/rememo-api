ec4c8ca266e84ffae7319097863748485b3361d6
Taras Rashkevych <taras.rashkevych1999@gmail.com>
2020-11-16 23:39:15 +0100

    feat: added get collections and get collection by id

--- source/api/v1/routers/collection.router.ts
+++ source/api/v1/routers/collection.router.ts
@@ -3,6 +3,16 @@ import * as Models from '../models';
 import databaseService from '../../../services/database.services';
 import { ObjectId } from 'mongodb';
 
+
+async function getCollections(req: express.Request, res: express.Response) {
+
+    const collections = await databaseService.getCollection('collections').find({owner: res.locals.username}).toArray();
+
+    return res.send(collections.map(col => Models.getCollectionFromDBDoc(col)));
+
+}
+
+
 async function createCollection(req: express.Request, res: express.Response) {
     const valCollection = Models.validateCollection(req.body);
     if (valCollection.error) {
@@ -17,10 +27,25 @@ async function createCollection(req: express.Request, res: express.Response) {
     }
 }
 
+async function getCollectionById(req: express.Request, res: express.Response){
+    const idColl = req.params.idColl;
+    if (!idColl) {
+        return res.status(404).send('No collection id found');
+    }
+    console.log(res.locals.username);
+    console.log(idColl);
+    const collection = await databaseService.getCollection('collections').findOne({_id: new ObjectId(idColl), owner: res.locals.username });
+    if (!collection) {
+        return res.status(404).send('No collection found');
+    } else {
+        return res.send(Models.getCollectionFromDBDoc(collection));
+    }
+}
+
 async function createWord(req: express.Request, res: express.Response) {
     const idColl = req.params.idColl;
     if (!idColl) {
-        return res.status(400).send('No collection id found');
+        return res.status(404).send('No collection id found');
     }
 
     const valWord = Models.validateWord(req.body);
@@ -41,7 +66,9 @@ async function createWord(req: express.Request, res: express.Response) {
 
 export default function (): express.Router {
     const router = express.Router();
+    router.get('/', getCollections);
     router.post('/', createCollection);
+    router.get('/:idColl', getCollectionById);
     router.post('/:idColl/words', createWord);
     return router;
 }
