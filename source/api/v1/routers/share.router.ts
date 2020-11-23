8833e5c1a31b707a7aa48cbb3052385d9b6a6162
alessiogandelli <alessiogandelli99@gmail.com>
2020-11-23 07:50:34 +0100

    fix: return json share collection

--- source/api/v1/routers/share.router.ts
+++ source/api/v1/routers/share.router.ts
@@ -2,9 +2,6 @@ import * as express from 'express';
 import * as Models from '../models';
 import LANG from '../../../lang';
 import * as collectionServices from '../services/collection.services';
-import { Collection } from '../models';
-
-
 
 async function shareCollectionById(req: express.Request, res: express.Response) {
     const idColl = req.params.idColl;
@@ -12,30 +9,28 @@ async function shareCollectionById(req: express.Request, res: express.Response) 
     if (!idColl) {
         return res.status(404).send(LANG.COLLECTION_ID_NOT_FOUND);
     }
-    collectionServices.updateCollectionById(idColl, res.locals.username, {share: true});
+    collectionServices.updateCollectionById(idColl, res.locals.username, { share: true });
+    const link = `api/v1/share/import/${idColl}`;
     
-    const link:string = 'api/v1/share/import/' + idColl;
-
-    res.send(link);
+    res.send({ url: link });
 
 }
 
 async function importCollection(req: express.Request, res: express.Response) {
     const idColl = req.params.idColl;
-    
+
     if (!idColl) {
         return res.status(404).send(LANG.COLLECTION_ID_NOT_FOUND);
     }
 
     const collection = await collectionServices.getCollectionById(idColl, res.locals.username);
-    
-    if(!collection?.share){
+
+    if (!collection?.share) {
         return res.status(404).send(LANG.COLLECTION_NOT_SHARED);
     }
 
 
     await collectionServices.createCollection(collection, res.locals._id, res.locals.username);
-
     return res.status(204).send();
 
 }
