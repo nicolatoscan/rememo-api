d8739d8f474ac87d50ac49dd6bcbc048ab6fb8ca
MattiaSalvador201506 <mattia.salvador@studenti.unitn.it>
2020-12-04 17:50:13 +0100

    feat(stats.services.ts): add save stats result function

--- source/api/v1/services/stats.services.ts
+++ source/api/v1/services/stats.services.ts
@@ -35,6 +35,37 @@ interface Result {
 }
 
 async function saveResult(result: Result[], userId: string) {
-    //salva nel db
+    const resultMappedByGroupId = result.reduce((entryMap, e) => entryMap.set(e.collectionId, [...entryMap.get(e.collectionId) || [], e]), new Map());
+    for (const collectionId of resultMappedByGroupId.keys()) {
+        const subResult = resultMappedByGroupId.get(collectionId) as Result[];
+        console.log(subResult);
+        const correctCollTest = subResult.filter(r => r.result && r.type === ResultType.Test).length;
+        const correctCollTrain = subResult.filter(r => r.result && r.type === ResultType.Train).length;
+        const wrongCollTest = subResult.filter(r => !r.result && r.type === ResultType.Test).length;
+        const wrongCollTrain = subResult.filter(r => !r.result && r.type === ResultType.Train).length;
+        const stat = await databaseHelper.getCollection('stats').findOne(
+            { collectionId: new ObjectId(collectionId), userId: userId },
+            //{ correctTrain: correctCollTrain.length, correctTest: correctCollTest.length, wrongTrain: wrongCollTrain.length, wrongTest: wrongCollTest.length}
+        ) as Models.DBStatsDoc | null;
+        if (stat) {
+            stat.wrongTest += wrongCollTest;
+            stat.correctTest += correctCollTest;
+            stat.wrongTrain += wrongCollTrain;
+            stat.correctTrain += correctCollTrain;
+            for(const wordState of subResult){
+                const word = stat.words.find(r => r.wordId.toHexString() === wordState.wordId); 
+                if (word){
+                    word.correctTest += ((wordState.result && wordState.type == ResultType.Test)? 1 : 0);
+                    word.wrongTest += ((!wordState.result && wordState.type == ResultType.Test)? 1 : 0);
+                    word.correctTrain += ((wordState.result && wordState.type == ResultType.Test)? 1 : 0);
+                    word.wrongTrain += ((!wordState.result && wordState.type == ResultType.Test)? 1 : 0);
+                }
+            }
+        }
+        await databaseHelper.getCollection('stats').replaceOne(
+            { collectionId: new ObjectId(collectionId), userId: userId },
+            stat
+        );
+    }
     return;
 }
